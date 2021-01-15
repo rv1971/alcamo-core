9879d1ef7ce919f5d383b99808aa212146ec478e
rv1971 <rv1971@web.de>
2021-01-15 12:57:24 +0100

    Add html_creation\element\Option

--- src/html_creation/element/Option.php
+++ src/html_creation/element/Option.php
@@ -6,4 +6,27 @@ use alcamo\html_creation\AbstractSpecificElement;
 
 class Option extends AbstractSpecificElement {
   const TAG_NAME = "option";
+
+  function __construct(
+    $value, $content = null, $compareTo = null, ?array $attrs = null
+  ) {
+    $attrs = compact( [ 'value' ] ) + (array)$attrs;
+
+    if ( isset( $compareTo ) ) {
+      switch ( true ) {
+        case is_callable( [ $compareTo, 'contains' ] ):
+          $attrs['selected'] = $compareTo->contains( $value );
+          break;
+
+        case is_array( $compareTo ):
+          $attrs['selected'] = in_array( $value, $compareTo, true );
+          break;
+
+        default:
+          $attrs['selected'] = $value == $compareTo;
+      }
+    }
+
+    parent::__construct( $content ?? $value, $attrs );
+  }
 }
