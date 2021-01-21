52dac1d988b09dd5f2e565d0e3f36f57ea4834f6
rv1971 <rv1971@web.de>
2021-01-21 17:42:01 +0100

    Add ietf\Lang

---
+++ src/ietf/Lang.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace alcamo\ietf;
+
+use alcamo\exception\{SyntaxError};
+
+/**
+ * @brief Language as in
+ * [RFC4646](http://tools.ietf.org/html/rfc4646).
+ *
+ * @invariant Immutable class.
+ *
+ * Only supports ISO 639 primary tags and ISO3166-1 region subtags.
+ */
+class Lang {
+  const PRIMARY_TAG_REGEXP = '/^[a-z]{2,3}$/';
+  const REGION_TAG_REGEXP  = '/^[A-Z]{2}$/';
+
+  private $primary_; ///< Primary tag.
+  private $region_;  ///< Region subtag.
+
+  public static function newFromString( string $string ) {
+    if ( isset( $string[2] ) ) {
+      return new self( substr( $string, 0, 2 ), substr( $string, 3 ) );
+    } else {
+      return new self( $string );
+    }
+  }
+
+  public function __construct( string $primary, ?string $region = null ) {
+    if ( !preg_match( static::PRIMARY_TAG_REGEXP, $primary ) ) {
+      throw new SyntaxError( $primary, null, '; not a valid ISO 639 language' );
+    }
+
+    $this->primary_ = $primary;
+
+    if ( isset( $region ) ) {
+      if ( !preg_match( static::REGION_TAG_REGEXP, $region ) ) {
+        throw new SyntaxError(
+          $region, null, '; not a valid ISO 3166-1 alpha-2 code' );
+      }
+
+      $this->region_ = $region;
+    }
+  }
+
+  public function getPrimary() : string {
+    return $this->primary_;
+  }
+
+  public function getRegion() : ?string {
+    return $this->region_;
+  }
+
+  /// Convert to [RFC 4646](http://tools.ietf.org/html/rfc4646) representation.
+  function __toString()
+  {
+    return isset( $this->region_ )
+      ? "{$this->primary_}-{$this->region_}" : $this->primary_;
+  }
+}
---
+++ tests/ietf/LangTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace alcamo\ietf;
+
+use PHPUnit\Framework\TestCase;
+
+use alcamo\exception\{SyntaxError};
+
+class LangTest extends TestCase {
+  /**
+   * @dataProvider constructProvider
+   */
+  public function testConstruct( $primary, $region, $expectedString ) {
+    $lang = new Lang( $primary, $region );
+
+    $this->assertSame( $primary, $lang->getPrimary() );
+    $this->assertSame( $region, $lang->getRegion() );
+
+    $this->assertEquals( $expectedString, (string)$lang );
+  }
+
+  public function constructProvider() {
+    return [
+      'without-region' => [ 'it', null, 'it' ],
+      'with-region' => [ 'en', 'US', 'en-US' ]
+    ];
+  }
+
+  public function testConstructPrimaryException() {
+    $this->expectException( SyntaxError::class );
+    $this->expectExceptionMessage(
+      'Syntax error in "hajara"; not a valid ISO 639 language' );
+
+    $comment = new Lang( 'hajara' );
+  }
+
+  public function testConstructRegionException() {
+    $this->expectException( SyntaxError::class );
+    $this->expectExceptionMessage(
+      'Syntax error in "X1"; not a valid ISO 3166-1 alpha-2 code' );
+
+    $comment = new Lang( 'en', 'X1' );
+  }
+
+  /**
+   * @dataProvider newFromStringProvider
+   */
+  public function testNewFromString(
+    $string, $expectedPrimary, $expectedRegion
+  ) {
+    $lang = Lang::newFromString( $string );
+
+    $this->assertSame( $expectedPrimary, $lang->getPrimary() );
+    $this->assertSame( $expectedRegion, $lang->getRegion() );
+  }
+
+  public function newFromStringProvider() {
+    return [
+      'without-region' => [ 'ee', 'ee', null ],
+      'with-region' => [ 'es-EC', 'es', 'EC' ]
+    ];
+  }
+}
