bed1820ca484cf05e1ab6e4a63c49f903f9911dc
rv1971 <rv1971@web.de>
2021-02-01 16:58:22 +0100

    Add xml\exception\UnknownNamespacePrefixTest

---
+++ src/xml/exception/UnknownNamespacePrefix.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace alcamo\xml\exception;
+
+use alcamo\exception\ValueException;
+
+/// Unknown namespace prefix
+class UnknownNamespacePrefix extends \UnexpectedValueException
+{
+    public $prefix;
+    public $validPrefixes;
+
+    /** If $message starts with a ';', it is appended to the generated message,
+     *  otherwise it replaces the generated one. */
+    public function __construct(
+        $prefix,
+        ?array $validPrefixes = null,
+        $message = null,
+        $code = 0,
+        \Exception $previous = null
+    ) {
+        $this->prefix = $prefix;
+        $this->validPrefixes = $validPrefixes;
+
+        if (!$message || $message[0] == ';') {
+            $message = "Invalid prefix \"$prefix\""
+                . (isset($validPrefixes)
+                   ? (", expected one of: \""
+                      . implode("\", \"", $validPrefixes) . '"')
+                   : '')
+                . $message;
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
---
+++ tests/xml/exception/UnknownNamespacePrefixTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace alcamo\xml\exception;
+
+use PHPUnit\Framework\TestCase;
+
+class UnknownNamespacePrefixTest extends TestCase
+{
+  /**
+   * @dataProvider constructProvider
+   */
+    public function testConstruct(
+        $prefix,
+        $validPrefixes,
+        $message,
+        $code,
+        $expectedMessage
+    ) {
+        $e = new UnknownNamespacePrefix(
+            $prefix,
+            $validPrefixes,
+            $message,
+            $code
+        );
+
+        $this->assertSame($prefix, $e->prefix);
+
+        $this->assertSame($validPrefixes, $e->validPrefixes);
+
+        $this->assertSame($expectedMessage, $e->getMessage());
+
+        $this->assertEquals($code, $e->getCode());
+    }
+
+    public function constructProvider(): array
+    {
+        return [
+            'typical-use' => [
+                'foo',
+                null,
+                null,
+                null,
+                'Invalid prefix "foo"'
+            ],
+
+            'custom-message' => [
+                'bar',
+                null,
+                'At vero eos et accusam',
+                43,
+                'At vero eos et accusam'
+            ],
+
+            'extra-message' => [
+                'baz',
+                [ 'foo', 'bar' ],
+                '; at vero eos et accusam',
+                44,
+                'Invalid prefix "baz", expected one of: "foo", "bar"; at vero eos et accusam'
+            ]
+        ];
+    }
+}
