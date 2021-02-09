1d545a10229e957f95c78a93ac194c824edeefe5
rv1971 <rv1971@web.de>
2021-02-09 15:29:12 +0100

    Fix xsd\Enumerator

--- src/decorator/DecoratorTrait.php
+++ src/decorator/DecoratorTrait.php
@@ -2,7 +2,12 @@
 
 namespace alcamo\decorator;
 
-/// Decorator pattern.
+/**
+ * @brief Decorator pattern.
+ *
+ * The various interfaces must be written down explicitely, otherwise PHP does
+ * not recognize that they are implemented.
+ */
 trait DecoratorTrait
 {
     protected $handler_; ///< Handler object.
@@ -35,5 +40,68 @@ trait DecoratorTrait
     public function __call($name, $params)
     {
         return call_user_func_array([ $this->handler_, $name ], $params);
+    }
+
+    /* == Countable interface == */
+
+    public function count()
+    {
+        return $this->handler_->count();
+    }
+
+    /* == Iterator interface == */
+
+    public function rewind()
+    {
+        return $this->handler_->rewind();
+    }
+
+    public function current()
+    {
+        return $this->handler_->current();
+    }
+
+    public function key()
+    {
+        return $this->handler_->key();
+    }
+
+    public function next()
+    {
+        return $this->handler_->next();
+    }
+
+    public function valid()
+    {
+        return $this->handler_->valid();
+    }
+
+    /* == IteratorAggregate interface == */
+
+    public function getIterator()
+    {
+        return $this->handler_->getIterator();
+    }
+
+    /* == ArrayAccess interface == */
+
+    public function offsetExists($offset)
+    {
+        return $this->handler_->offsetExists($offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->handler_->offsetGet($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        return $this->handler_->offsetSet($offset, $value);
+    }
+
+    public function offsetUnset($offset)
+    {
+        return $this->handler_->offsetUnset($offset);
     }
 }
