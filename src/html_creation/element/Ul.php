224a986b0aeb02079f0de4d7cfd7bdd7f3aa60d4
rv1971 <rv1971@web.de>
2021-02-24 09:58:08 +0100

    Code formatting

--- src/html_creation/element/Ul.php
+++ src/html_creation/element/Ul.php
@@ -6,17 +6,18 @@ class Ul extends AbstractSpecificElement
 {
     public const TAG_NAME = "ul";
 
-  /** Wrap each item into an Li if it is not yet an element allowed within
-   * TAG_NAME. */
+    /** Wrap each item into an Li if it is not yet an element allowed within
+     * TAG_NAME. */
     public function __construct(iterable $items, ?iterable $attrs = null)
     {
         $content = [];
 
         foreach ($items as $item) {
             $content[] =
-            $item instanceof Li || $item instanceof AbstractScriptSupportingElement
-            ? $item
-            : new Li($item);
+                ($item instanceof Li
+                 || $item instanceof AbstractScriptSupportingElement)
+                ? $item
+                : new Li($item);
         }
 
         parent::__construct($content, $attrs);
