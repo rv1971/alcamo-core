0cca1767c51e4e5bc43a94d5174691c242b80db7
rv1971 <rv1971@web.de>
2021-02-26 14:48:13 +0100

    Add base URL to TrivialRelativeUrlFactory

--- src/html_creation/TrivialRelativeUrlFactory.php
+++ src/html_creation/TrivialRelativeUrlFactory.php
@@ -5,10 +5,27 @@ namespace alcamo\html_creation;
 // Class returning a relative path unchanged
 class TrivialRelativeUrlFactory extends AbstractUrlFactory
 {
+    private $baseUrl_; ///< URL to prepend
+
+    public function __construct(?string $baseUrl = null)
+    {
+        $this->baseUrl_ = $baseUrl;
+    }
+
+    public function getBaseUrl(): ?string
+    {
+        return $this->baseUrl_;
+    }
+
+    /**
+     * @warning It is not checked whether the supplied path is actually
+     * relative.
+     */
     public function createFromPath(string $path): string
     {
-        return DIRECTORY_SEPARATOR == '/'
-            ? $path
-            : str_replace(DIRECTORY_SEPARATOR, '/', $path);
+        return $this->baseUrl_
+            . (DIRECTORY_SEPARATOR == '/'
+               ? $path
+               : str_replace(DIRECTORY_SEPARATOR, '/', $path));
     }
 }
--- tests/html_creation/TrivialRelativeUrlFactoryTest.php
+++ tests/html_creation/TrivialRelativeUrlFactoryTest.php
@@ -10,8 +10,24 @@ class TrivialRelativeUrlFactoryTest extends TestCase
     {
         $factory = new TrivialRelativeUrlFactory();
 
+        $this->assertNull($factory->getBaseUrl());
+
         $this->assertSame(
             'foo/bar/baz',
+            $factory->createFromPath(
+                'foo' . DIRECTORY_SEPARATOR . 'bar' . DIRECTORY_SEPARATOR . 'baz'
+            )
+        );
+
+        $factory = new TrivialRelativeUrlFactory('https://www.example.org/');
+
+        $this->assertSame(
+            'https://www.example.org/',
+            $factory->getBaseUrl()
+        );
+
+        $this->assertSame(
+            'https://www.example.org/foo/bar/baz',
             $factory->createFromPath(
                 'foo' . DIRECTORY_SEPARATOR . 'bar' . DIRECTORY_SEPARATOR . 'baz'
             )
