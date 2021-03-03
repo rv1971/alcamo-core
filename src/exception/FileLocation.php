849093ed97d65549f63fa316fdbc5883a2ee6acd
rv1971 <rv1971@web.de>
2021-03-03 13:23:10 +0100

    Add FileLocation

---
+++ src/exception/FileLocation.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace alcamo\exception;
+
+/// Location in file
+class FileLocation
+{
+    private $filename_; ///< Something that can be converted to string
+    private $line_;     ///< ?int
+    private $column_;   ///< ?int
+
+    public static function newFromThrowable(\Throwable $throwable)
+    {
+        return new static($throwable->getFile(), $throwable->getLine());
+    }
+
+    public static function newFromBacktraceItem(array $frame)
+    {
+        return new static($frame['file'] ?? null, $frame['line'] ?? null);
+    }
+
+    public function __construct(
+        $filename,
+        ?int $line = null,
+        ?int $column = null
+    ) {
+        $this->filename_ = $filename;
+        $this->line_ = $line;
+        $this->column_ = $column;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename_;
+    }
+
+    public function getLine(): ?int
+    {
+        return $this->line_;
+    }
+
+    public function getColumn(): ?int
+    {
+        return $this->column_;
+    }
+
+    public function __toString()
+    {
+        $result = [];
+
+        if ($this->filename_) {
+            $result[] = $this->filename_;
+        }
+
+        if ($this->line_) {
+            $result[] = $this->line_;
+        }
+
+        if ($this->column_) {
+            if ($result) {
+                $result[] = $this->column_;
+            } else {
+                $result[] = "column $this->column_";
+            }
+        }
+
+        return implode(':', $result);
+    }
+}
