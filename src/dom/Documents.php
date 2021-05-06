549a4e86a22733ec0d16daf6a296d23009df78cb
rv1971 <rv1971@web.de>
2021-05-06 14:08:21 +0200

    Add dom\Documents\newFromUrls()

--- src/dom/Documents.php
+++ src/dom/Documents.php
@@ -3,6 +3,7 @@
 namespace alcamo\dom;
 
 use alcamo\collection\ReadonlyCollection;
+use GuzzleHttp\Psr7\{Uri, UriResolver};
 
 /// Array of DOM documents indexed by dc:identifier.
 class Documents extends ReadonlyCollection
@@ -25,6 +26,43 @@ class Documents extends ReadonlyCollection
 
             if ($key === '') {
                 $key = pathinfo($path, PATHINFO_FILENAME);
+            }
+
+            $docs[$key] = $doc;
+        }
+
+        return new self($docs);
+    }
+
+    public static function newFromUrls(
+        iterable $urls,
+        $baseUrl = null,
+        ?int $libXmlOptions = null
+    ): self {
+        $docs = [];
+
+        if (isset($baseUrl) && !($baseUrl instanceof Uri)) {
+            $baseUrl = new Uri((string)$baseUrl);
+        }
+
+        foreach ($urls as $url) {
+            if (!($url instanceof Uri)) {
+                $url = new Uri((string)$url);
+            }
+
+            if (isset($baseUrl)) {
+                $url = UriResolver::resolve($baseUrl, $url);
+            }
+
+            $doc = self::createDocumentFromUrl($url, $libXmlOptions);
+
+            $key = $doc->documentElement->getAttributeNS(
+                Document::NS['dc'],
+                'identifier'
+            );
+
+            if ($key === '') {
+                $key = pathinfo($url->getPath(), PATHINFO_FILENAME);
             }
 
             $docs[$key] = $doc;
--- tests/dom/DocumentsTest.php
+++ tests/dom/DocumentsTest.php
@@ -4,6 +4,7 @@ namespace alcamo\dom;
 
 use PHPUnit\Framework\TestCase;
 use alcamo\exception\Uninitialized;
+use alcamo\ietf\Uri;
 
 class DocumentsTest extends TestCase
 {
@@ -24,6 +25,36 @@ class DocumentsTest extends TestCase
     {
         $docs =
             Documents::newFromGlob(__DIR__ . DIRECTORY_SEPARATOR . '*.xml');
+
+        $this->assertSame('foo', $docs['foo']->documentElement->tagName);
+
+        $this->assertSame('bar', $docs['bar-bar']->documentElement->tagName);
+    }
+
+    public function testNewFromUrls()
+    {
+        $docs = Documents::newFromUrls(
+            [ 'foo.xml', new Uri('bar.xml') ],
+            'file://' . str_replace(DIRECTORY_SEPARATOR, '/', __DIR__) . '/'
+        );
+
+        $this->assertSame('foo', $docs['foo']->documentElement->tagName);
+
+        $this->assertSame('bar', $docs['bar-bar']->documentElement->tagName);
+    }
+
+    public function testNewFromUrlsAbs()
+    {
+        $docs = Documents::newFromUrls(
+            [
+                new Uri(
+                    'file://' . str_replace(DIRECTORY_SEPARATOR, '/', __DIR__)
+                    . '/foo.xml'
+                ),
+                'file://' . str_replace(DIRECTORY_SEPARATOR, '/', __DIR__)
+                . '/bar.xml'
+            ]
+        );
 
         $this->assertSame('foo', $docs['foo']->documentElement->tagName);
 
