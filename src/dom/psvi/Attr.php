f41e5e91b50450d7529a806d5d640c8c8fe79242
rve <r.verhoeven@weat.de>
2021-05-07 19:43:22 +0200

    Add information to exceptions thrown in dom\psvi\Attr::createValue()

--- src/dom/psvi/Attr.php
+++ src/dom/psvi/Attr.php
@@ -45,67 +45,76 @@ class Attr extends BaseAttr
 
     protected function createValue()
     {
-        $attrType = $this->getType();
+        try {
+            $attrType = $this->getType();
 
-        $converters = $this->ownerDocument->getAttrConverters();
+            $converters = $this->ownerDocument->getAttrConverters();
 
-        $converter = $converters->lookup($attrType);
+            $converter = $converters->lookup($attrType);
 
-        /* If there is a specific converter, use it. */
-        if (isset($converter)) {
-            return $converter($this->value, $this);
-        }
-
-        /* Otherwise, if the type is a list type, convert the value to a
-         * numerically-indexed array. */
-        if ($attrType instanceof ListType) {
-            $value = preg_split('/\s+/', $this->value);
-
-            $itemType = $attrType->getItemType();
-            $converter = $converters->lookup($itemType);
-
-            /* In particular, if the type is a list type and there is a
-             * converter for the item type, replace the value by an
-             * associative array, mapping each item literal to its conversion
-             * result.
-             *
-             * @warning This implies that repeated items will silently be
-             * dropped in this case. To model lists of items having a
-             * converter with possible repetition, an explicit converter for
-             * the list type is necessary.
-             */
+            /* If there is a specific converter, use it. */
             if (isset($converter)) {
-                $convertedValue = [];
-
-                foreach ($value as $item) {
-                    $convertedValue[$item] = $converter($item, $this);
-                }
-
-                return $convertedValue;
+                return $converter($this->value, $this);
             }
 
-            /* Otherwise, if the type is a list type and the items are
-             * enumerators, replace the value by an associative array, mapping
-             * each item literal to its enumerator object.
-             *
-             * @warning This implies that repeated enumerators will silently
-             * be dropped. To model lists of enumerators with possible
-             * repetition, an explicit converter for the list type is
-             * necessary.
+            /* Otherwise, if the type is a list type, convert the value to a
+             * numerically-indexed array. */
+            if ($attrType instanceof ListType) {
+                $value = preg_split('/\s+/', $this->value);
+
+                $itemType = $attrType->getItemType();
+                $converter = $converters->lookup($itemType);
+
+                /* In particular, if the type is a list type and there is a
+                 * converter for the item type, replace the value by an
+                 * associative array, mapping each item literal to its
+                 * conversion result.
+                 *
+                 * @warning This implies that repeated items will silently be
+                 * dropped in this case. To model lists of items having a
+                 * converter with possible repetition, an explicit converter
+                 * for the list type is necessary.
+                 */
+                if (isset($converter)) {
+                    $convertedValue = [];
+
+                    foreach ($value as $item) {
+                        $convertedValue[$item] = $converter($item, $this);
+                    }
+
+                    return $convertedValue;
+                }
+
+                /* Otherwise, if the type is a list type and the items are
+                 * enumerators, replace the value by an associative array,
+                 * mapping each item literal to its enumerator object.
+                 *
+                 * @warning This implies that repeated enumerators will
+                 * silently be dropped. To model lists of enumerators with
+                 * possible repetition, an explicit converter for the list
+                 * type is necessary.
              */
-            if ($itemType instanceof EnumerationTypeInterface) {
-                $convertedValue = [];
+                if ($itemType instanceof EnumerationTypeInterface) {
+                    $convertedValue = [];
 
-                foreach ($value as $item) {
-                    $convertedValue[$item] = $itemType->getEnumerators()[$item];
+                    foreach ($value as $item) {
+                        $convertedValue[$item] = $itemType->getEnumerators()[$item];
+                    }
+
+                    return $convertedValue;
                 }
 
-                return $convertedValue;
+                return $value;
             }
 
-            return $value;
+            return parent::createValue();
+        } catch (\Throwable $e) {
+            $e->name = $this->name;
+            $e->documentURI = $this->ownerDocument->documentURI;
+            $e->lineNo = $this->getLineNo();
+
+            throw $e;
         }
 
-        return parent::createValue();
     }
 }
