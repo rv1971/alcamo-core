1edf681b72c23ac82c90a2ced9ab80ba03fdaf5a
rv1971 <rv1971@web.de>
2021-05-26 11:37:34 +0200

    Validate XSDs if xsi:schemaLocation provided

--- src/dom/xsd/Document.php
+++ src/dom/xsd/Document.php
@@ -2,11 +2,13 @@
 
 namespace alcamo\dom\xsd;
 
-use alcamo\dom\DocumentFactoryInterface;
+use alcamo\dom\{DocumentFactoryInterface, ValidationTrait};
 use alcamo\dom\extended\{Document as BaseDocument, DocumentFactory};
 
 class Document extends BaseDocument
 {
+    use ValidationTrait;
+
     public const NODE_CLASS =
         [
             'DOMAttr'    => Attr::class,
