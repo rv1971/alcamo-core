d48e081fd7be7b9c68946fcc9d39d67bf975596a
rv1971 <rv1971@web.de>
2021-06-14 19:45:13 +0200

    Review and document namespace modular_class

--- src/modular_class/ModuleTrait.php
+++ src/modular_class/ModuleTrait.php
@@ -2,11 +2,22 @@
 
 namespace alcamo\modular_class;
 
+/**
+ * @brief Trait for a class module
+ *
+ * @attention Any class using this trait MUST define a public constant `NAME`
+ * telling the module's name.
+ *
+ * This trait is a decorator for the parent object.
+ *
+ * @date Last reviewed 2021-06-14
+ */
 trait ModuleTrait
 {
-    protected $parent_;
+    protected $parent_; ///< Parent object containing this module
 
-    public function init($parent)
+    /// Called by the parent's addModule() method.
+    public function init($parent): void
     {
         $this->parent_ = $parent;
     }
--- src/modular_class/ParentTrait.php
+++ src/modular_class/ParentTrait.php
@@ -4,17 +4,34 @@ namespace alcamo\modular_class;
 
 use alcamo\collection\ReadonlyCollectionTrait;
 
+/**
+ * @brief Trait for creating a modular class
+ *
+ * @attention Any modules added to an object of this class must use
+ * ModuleTrait.
+ *
+ * Modules added to this class can be accessed via the ArrayAccess interface
+ * and behave as if they were derived from this class. This is useful if
+ * a class would have a very large number of features, and there might even be
+ * name clashes between members. Subdividing the functionality into modules
+ * helps implementing the single-responsibility principle.
+ *
+ * @date Last reviewed 2021-06-14
+ */
 trait ParentTrait
 {
     use ReadonlyCollectionTrait;
 
-    public function addModule($module)
+    /// Add a module object
+    public function addModule($module): void
     {
+        /** Upon addition, call the module's init() method with `$this` as its
+         *  argument, this making the parent object known to the module. */
         $module->init($this);
         $this->data_[$module::NAME] = $module;
     }
 
-    public function addModules(iterable $modules)
+    public function addModules(iterable $modules): void
     {
         foreach ($modules as $module) {
             $this->addModule($module);
